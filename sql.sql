8861df9cd9bbb510b46cb64a418d80f932f6ee49
tuxiantian <tuxiantian@163.com>
2024-10-08 22:01:31 +0800

    打包一个完整的aph app,方便使用

--- sql.sql
+++ sql.sql
@@ -9,3 +9,8 @@ CREATE TABLE `ahp_history` (
   `alternative_names` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--示例数据
+INSERT INTO test.ahp_history
+(id, request_data, response_data, created_at, criteria_names, alternative_names)
+VALUES(3, '{"criteria_names": ["景色", "吃住", "价格"], "criteria_matrix": [["1", "1/3", "3"], ["3", "1", "5"], ["1/3", "1/5", "1"]], "alternative_names": ["北京", "南京", "杭州"], "alternative_matrices": [[["1", "2", "5"], ["1/2", "1", "3"], ["1/5", "1/3", "1"]], [["1", "1/4", "3"], ["4", "1", "7"], ["1/3", "1/7", "1"]], [["1", "5", "1/3"], ["1/5", "1", "1/7"], ["3", "7", "1"]]]}', '{"priority_vector": [0.3164966052987531, 0.532616811758014, 0.15088658294323287], "best_choice_name": "南京"}', '2024-10-04 23:33:57', '景色,吃住,价格', '北京,南京,杭州');
